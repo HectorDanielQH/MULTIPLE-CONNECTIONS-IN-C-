d2145871c8f29dd5a30093e31c079a3a495942b6
HectorDanielQH <hdaniel369@gmail.com>
2022-01-20 17:27:11 -0400

    PRIMERA INTERACCION

---
+++ CONEXIONES/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace CONEXIONES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CONEXION A SQL MICROSOFT 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(272, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CONEXION A MYSQL 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(528, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 54);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "CONEXION A POSTGRES 1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 171);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(408, 235);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(426, 171);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 54);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "SIGUIENTE";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(554, 171);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(122, 54);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "ATRAS";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(554, 231);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(122, 54);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "FIN";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(426, 231);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(122, 54);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "PRINCIPIO";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(426, 306);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(426, 384);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(426, 332);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(250, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(426, 358);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(250, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(12, 97);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(148, 54);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "CONEXION A SQL MICROSOFT 2";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(528, 97);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(148, 54);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "CONEXION A POSTGRES 2";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(272, 97);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(148, 54);
+            this.button12.TabIndex = 14;
+            this.button12.Text = "CONEXION A MYSQL 2";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 418);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ CONEXIONES/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using Npgsql;
+using System.Configuration;
+using MongoDB.Driver;
+
+namespace CONEXIONES
+{
+    public partial class Form1 : Form
+    {
+        private int cotrl=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["SQL"].ToString());
+            SqlCommand comando = new SqlCommand("select * from usuario", conexion);
+            comando.CommandType = CommandType.Text;
+            DataSet datos = new DataSet();
+            conexion.Open();
+            SqlDataAdapter adapta = new SqlDataAdapter(comando);
+            adapta.Fill(datos);
+            conexion.Close();
+            bindingSource1.DataSource = datos.Tables[0];
+            dataGridView1.DataSource = bindingSource1;
+            if (cotrl == 0)
+            {
+                textBox1.DataBindings.Add("Text", bindingSource1, "CI");
+                textBox2.DataBindings.Add("Text", bindingSource1, "NOMBRE");
+                textBox3.DataBindings.Add("Text", bindingSource1, "APELLIDO");
+                textBox4.DataBindings.Add("Text", bindingSource1, "EDAD");
+            }
+            cotrl = 1;
+            MessageBox.Show("se cargo correctamente");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conex = new MySqlConnection(ConfigurationManager.ConnectionStrings["MYSQL"].ToString());
+            MySqlCommand comando = new MySqlCommand("select * from usuario;", conex);
+            comando.CommandType = CommandType.Text;
+            DataSet datos = new DataSet();
+            conex.Open();
+            MySqlDataAdapter adapta = new MySqlDataAdapter(comando);
+            adapta.Fill(datos);
+            conex.Close();
+            DataTable tab = new DataTable();
+            tab = datos.Tables[0];
+            MessageBox.Show(tab.PrimaryKey.ToString());
+            bindingSource1.DataSource = datos.Tables[0];
+            dataGridView1.DataSource = bindingSource1;
+            MessageBox.Show("se cargo correctamente");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection cone = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["POSTGRES"].ToString());
+            NpgsqlCommand comando = new NpgsqlCommand("select * from usuario;", cone);
+            comando.CommandType = CommandType.Text;
+            DataSet datos = new DataSet();
+            cone.Open();
+            NpgsqlDataAdapter adapta = new NpgsqlDataAdapter(comando);
+            adapta.Fill(datos);
+            cone.Close();
+            bindingSource1.DataSource = datos.Tables[0];
+            dataGridView1.DataSource = bindingSource1;
+            MessageBox.Show("se cargo correctamente");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
